931b72ab1d5cdfe957bcac642113667f2ae37807
L1LSunflower <miserablelilsunflower@gmail.com>
2022-09-13 16:39:23 +0600

    added success solution to AddStrings

--- AddStrings/main.go
+++ AddStrings/main.go
@@ -3,31 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	num1, num2 := "12341341234", "532323452"
+	num1, num2 := "1", "9"
 	fmt.Println(addStrings(num1, num2))
 }
 
 func addStrings(num1, num2 string) string {
+	if len(num1) > len(num2) {
+		return sumStringValues(num1, num2)
+	} else {
+		return sumStringValues(num2, num1)
+	}
+}
+
+func sumStringValues(num1, num2 string) string {
 	var overflow bool
 	var tempRes int
-	if len(num1) >= len(num2) {
-		numRunes := []rune(num1)
-		for i := 0; i <= len(num2)-1; i++ {
-			if overflow {
-				tempRes = convertByteToInt(numRunes[len(numRunes)-1-i]) + convertByteToInt(rune(num2[len(num2)-1-i])) + 1
-				overflow = false
-			} else {
-				tempRes = convertByteToInt(numRunes[len(numRunes)-1-i]) + convertByteToInt(rune(num2[len(num2)-1-i]))
-			}
-			if tempRes >= 10 {
-				overflow = true
-				tempRes -= 10
-			}
-			numRunes[len(numRunes)-1-i] = rune(tempRes + '0')
-		}
+	numRunes := []rune(num1)
+	for i := 0; i < len(num2) || overflow == true; i++ {
 		//fmt.Println(numRunes)
-		num1 = string(numRunes)
+		if overflow {
+			if i >= len(num2) && i < len(numRunes) {
+				tempRes = convertByteToInt(numRunes[len(numRunes)-1-i]) + 1
+			} else if i >= len(numRunes) {
+				return "1" + string(numRunes)
+			} else {
+				tempRes = convertByteToInt(numRunes[len(numRunes)-1-i]) + convertByteToInt(rune(num2[len(num2)-1-i])) + 1
+			}
+			overflow = false
+		} else {
+			tempRes = convertByteToInt(numRunes[len(numRunes)-1-i]) + convertByteToInt(rune(num2[len(num2)-1-i]))
+		}
+		if tempRes >= 10 {
+			overflow = true
+			tempRes -= 10
+		}
+		numRunes[len(numRunes)-1-i] = rune(tempRes + '0')
 	}
+	num1 = string(numRunes)
+
 	return num1
 }
 
